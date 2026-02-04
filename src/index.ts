58e841d2e732fd0a110f2482fbe64d98e1776b4c
CharlesMwaura <94879757+cmwrxh@users.noreply.github.com>
2026-02-04 21:53:15 +0300

    feat: add 'add' command with in-memory storage

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,20 @@
 import { Command } from 'commander';
 
 const program = new Command();
+let tasks: string[] = [];
 
 program
   .name('taskforge')
   .description('CLI task manager')
   .version('1.0.0');
+
+program
+  .command('add')
+  .description('Add a task')
+  .argument('<task>', 'task description')
+  .action((task) => {
+    tasks.push(task);
+    console.log(`Added: ${task}`);
+  });
 
 program.parse();
